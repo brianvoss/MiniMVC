f41c23d913f90b625aad1d9216515a8f31a2accf
Brian Voss <brian@brianvoss.com>
2012-05-06 15:39:43 -0700

    Added default view template

---
+++ templates/main.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	<meta charset="utf-8" />
+	<title><?php echo $this->lang["title"]; ?></title>
+	<link rel="stylesheet" href="/css/app.css" />
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js" type="text/javascript"></script>
+	<script type="text/javascript">
+	    function setup(){
+		window.application = new Application();
+	    }
+	</script>
+    </head>
+    <body>
+        <div id="site_outer">
+            <h1>MiniMVC - a small no-nonsense php framework</h1>
+        </div>
+        
+        <script src="/js/app.js" type="text/javascript"></script>
+    </body>
+</html>
